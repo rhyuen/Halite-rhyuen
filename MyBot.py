386c4d1d2e0d5282ff1e38bad4a698a1afcd4f2f
Robert Yuen <dungeonry@hotmail.com>
2016-12-17 15:12:38 -0800

    Improved bot -> Patientbot

--- MyBot.py
+++ MyBot.py
@@ -6,15 +6,39 @@ import random
 myID, game_map = hlt.get_init()
 hlt.send_init("rhyuenBot")
 
-def assign_move(square):
-    for direction, neighbor in enumerate(game_map.neighbors(square)):
-        if neighbor.owner != myID and neighbor.strength < square.strength:
-            return Move(square, direction)
+def findNearestOppDirection(square):
+    direction = NORTH
+    maxDistance = min(game_map.width, game_map.height)/2
 
+    #start loop
+    distance = 0
+    current = square
+    site = game_map.get
+
+
+
+    return direction
+
+
+def assign_move(square):
+    border = False
+    ###On Border, Can I Win?
+    for direction, neighbor in enumerate(game_map.neighbors(square)):
+        if neighbor.owner != myID:
+            border = True
+            if neighbor.strength < square.strength:
+                return Move(square, direction)
+
+    ###Not enough power, build it.
     if square.strength < 5 * square.production:
         return Move(square, STILL)
-    else:
+
+    ###Not on border, advance territory.
+    if border == False:
         return Move(square, random.choice((NORTH, WEST)))
+
+    #On Border, but can't win. Hold position.
+    return Move(square, STILL)
 
 while True:
     game_map.get_frame()
---
+++ README.md
@@ -0,0 +1,15 @@
+### About
+
+> code for a contest of sorts on https://halite.io .
+
+### My Ranking
+
+> It's [here](https://halite.io/user.php?userID=4070).  Just a lowly bronze leaguer.
+
+### Making it go on Windows
+
+> \> .\runGame.bat
+
+### Making it go on Linux
+
+>  $ ./runGame.sh
